382b18c9044897155f9274128091e12cbe9129db
xjunw <awesomejay180@gmail.com>
2026-07-09 03:26:54 -0700

    Added Abstraction (Counting Effect)

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 window.onload = async (event) => {
     document.body.style.display = "flex";
-    let checkLifespan = document.querySelector("#lifespan");
-    let checkSpecies = document.querySelector("#species");
+    // Checks which span element exists
+    let checkLifespan = await document.querySelector("#lifespan");
+    let checkSpecies = await document.querySelector("#species");
     if (checkLifespan) {
-        let lifespan = document.getElementById("lifespan").innerHTML;
-        for (let i = 0; i <= lifespan; i++) {
-            document.getElementById("lifespan").innerHTML = i;
-            await new Promise(resolve => setTimeout(resolve, 1000 / lifespan));
-        }
+        countingEffect("lifespan");
     }
     if (checkSpecies) {
-        let species = document.getElementById("species").innerHTML;
-        for (let i = 0; i <= species; i += 100) {
-            document.getElementById("species").innerHTML = i;
-            await new Promise(resolve => setTimeout(resolve, 0));
-        }
+        countingEffect("species");
+    }
+}
+
+async function countingEffect(_element) {
+    let a = document.getElementById(_element).innerHTML;
+        for (let i = 0; i <= a; i += a / 40) {
+            document.getElementById(_element).innerHTML = Math.round(i);
+            await new Promise(resolve => setTimeout(resolve, 25));
     }
 }
