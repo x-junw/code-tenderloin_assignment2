19ce9b9af9fd7bb13faaaa552b0e06ac0a036d19
xjunw <awesomejay180@gmail.com>
2026-07-09 04:02:53 -0700

    Added Popout Effect

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 window.onload = async (event) => {
     document.body.style.display = "flex";
+    await countingStep();
+    await popupStep();
+}
+
+function countingStep() {
     // Checks which span element exists
-    let checkLifespan = await document.querySelector("#lifespan");
-    let checkSpecies = await document.querySelector("#species");
+    let checkLifespan = document.querySelector("#lifespan");
+    let checkSpecies = document.querySelector("#species");
     if (checkLifespan) {
         countingEffect("lifespan");
     }
@@ -13,8 +18,21 @@ window.onload = async (event) => {
 
 async function countingEffect(_element) {
     let a = document.getElementById(_element).innerHTML;
-        for (let i = 0; i <= a; i += a / 40) {
+    for (let i = 0; i <= a; i += a / 40) {
             document.getElementById(_element).innerHTML = Math.round(i);
             await new Promise(resolve => setTimeout(resolve, 25));
     }
+    document.getElementById(_element).innerHTML = a;
+}
+
+function popupStep() {
+    const info = document.querySelectorAll(".info");
+    info.forEach(async (element) => {
+        let x = element.innerHTML;
+        for(let i = 0; i <= x.length; i++) {
+            element.innerHTML = x;
+            element.innerHTML = element.innerHTML.slice(0, i);
+            await new Promise(resolve => setTimeout(resolve, 500 / x.length));
+        }
+    });
 }
